5eddf463d1d7dde291398b48c0c083b3eb0e9d3f
kristiansantos11 <57390925+kristiansantos11@users.noreply.github.com>
2020-06-01 15:26:58 +0800

    Added profile pages to the first four people in the member-list.

--- members/js/member-list.js
+++ members/js/member-list.js
@@ -1,3 +1,20 @@
-document.getElementsByClassName("desc-popup").onClick = function() {
-    location.href = "/"
-}
+const signusfalcon = document.querySelector("#signusfalcon");
+const rj0001 = document.querySelector("#rj0001");
+const thisisausername = document.querySelector("#thisisausername");
+const heylale = document.querySelector("#heylale");
+
+signusfalcon.addEventListener('click', () => {
+    window.location.href = "/members/signusfalcon";
+});
+
+rj0001.addEventListener('click', () => {
+    window.location.href = "/members/rj0001";
+});
+
+thisisausername.addEventListener('click', () => {
+    window.location.href = "/members/thisisausername";
+});
+
+heylale.addEventListener('click', () => {
+    window.location.href = "/members/heylale";
+});
